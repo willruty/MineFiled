51f380314ea3c5a89131a11f3a79da08313a7215
willruty <williamrutynamacedo@gmail.com>
2025-02-05 13:42:10 -0300

    sla

--- src/br/com/Cod3r/cm/modelo/CampoEvento.java
+++
@@ -1,6 +0,0 @@
-package br.com.Cod3r.cm.modelo;
-
-public enum CampoEvento {
-
-	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
-}
--- src/br/com/Cod3r/cm/modelo/CampoObservador.java
+++
@@ -1,6 +0,0 @@
-package br.com.Cod3r.cm.modelo;
-
-public interface CampoObservador {
-
-	public void eventoOcorreu(Campo campo, CampoEvento evento);
-}
